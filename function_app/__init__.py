2808a9325b0d8ac0b195862af3d8a1df3b52f2ab
erabhishek1989-pixel <er.abhishek1989@gmail.com>
2026-05-18 20:37:55 +0100

    chore: restart function app after terraform config changes

--- function_app/__init__.py
+++ function_app/__init__.py
@@ -1,2 +1,3 @@
 # Entry point moved to function_app.py (required name for Python v2 programming model).
 # Azure Functions worker discovers functions via function_app.py, not __init__.py.
+# restarted Mon May 18 20:37:55 GMTDT 2026
